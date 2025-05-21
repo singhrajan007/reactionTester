5e0385d0b8369135af95941f4d7485b63818da7d
Rajan Singh <76697406+singhrajan007@users.noreply.github.com>
2025-05-21 20:20:53 +0530

    Update script.js
    avoiding rotation of box

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ function rotate() {
     document.getElementById("reactionTime").style.position = "absolute";
     document.getElementById("reactionTime").style.width = randomwidthHeight + "px";
     document.getElementById("reactionTime").style.height = randomwidthHeight + "px";
-    document.getElementById("reactionTime").style.transform = "rotate(" + Math.floor(Math.random() * 360) + "deg)";
     document.getElementById("reactionTime").style.backgroundColor = "hsl(" + Math.floor(Math.random() * 360) + ", 100%, 50%)";
     document.getElementById("reactionTime").style.marginLeft=randomX + "px";
     document.getElementById("reactionTime").style.marginTop=randomY + "px";
